373360e16f411255ce8de27a052a105d7ea43fe4
Ivan <js314head@gmail.com>
2020-03-26 12:30:28 +0100

    Završen vizual

--- program.js
+++ program.js
@@ -1,17 +1,33 @@
-let spremnik;
+let spremnik = [];
+
 
 document.querySelector('.plus').addEventListener('click', function(e) {
-    spremnik = document.querySelector('.numInput').value
+    
+    let noviBroj = document.querySelector('.numInput').value
+    spremnik.push(noviBroj)
+    
+    document.querySelector('.jednako').style.backgroundColor = 'red';
 
-    console.log(spremnik)
+ 
 })
 
+
+document.querySelector('.jednako').addEventListener('click', function(e) {
+    let reza = 0;
+    for(let i = 0; i++; i < spremnik.length) {
+        reza += spremnik[i]
+    }
+    
+    document.querySelector('.numInput').value = `= ${reza}`;
+})
 
 
 
 document.querySelector('.reset').addEventListener('click', function(e) {
-    spremnik = 0;
+    spremnik = [];
+    document.querySelector('.numInput').value = '';
 })
 
 
 
+ 
